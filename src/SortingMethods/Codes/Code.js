faba7ea981c594bfc3727b3be7b0f30bab177061
NIVAS BODAPATI <nivasbodapati.abc@gmail.com>
2023-05-22 15:36:06 +0530

    Changed Spell mistake of algorithm

--- src/SortingMethods/Codes/Code.js
+++ src/SortingMethods/Codes/Code.js
@@ -5,7 +5,7 @@ import Codes from '../codes.json';
 const BubbleSortCode = ({code, name}) => {
     const lines = Codes[code];
     return <>
-    <h2 className='code-heading'>{name} Algotirhm Code</h2>
+    <h2 className='code-heading'>{name} Algorithm Code</h2>
     <div className='outer-container'>
     <div className="container">
         {lines.map((line, idx) => {
